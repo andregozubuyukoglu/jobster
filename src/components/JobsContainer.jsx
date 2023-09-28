565cb0c2e71f20fd5f646d43fbab013e0bf7dd05
Andre <andregozubuyuk@gmail.com>
2023-09-29 00:18:49 +0300

    Updated files

--- src/components/JobsContainer.jsx
+++ src/components/JobsContainer.jsx
@@ -1,3 +1,8 @@
+import { useEffect } from "react"
+import Job from "./Job"
+import Wrapper from "../assets/wrappers/JobsContainer"
+import { useSelector, useDispatch } from "react-redux"
+
 const JobsContainer = () => {
   return <div>JobsContainer</div>
 }
