14bd4551333cd67e40b97bded1dd22f3a499161f
Andre <andregozubuyuk@gmail.com>
2023-09-28 23:31:59 +0300

    Updated files

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,18 @@ import Navbar from "./Navbar"
 import SmallSidebar from "./SmallSidebar"
 import BigSidebar from "./BigSidebar"
 import FormRowSelect from "./FormRowSelect"
+import JobsContainer from "./JobsContainer"
+import SearchContainer from "./SearchContainer"
+import Job from "./Job"
 
-export { Logo, FormRow, Navbar, SmallSidebar, BigSidebar, FormRowSelect }
+export {
+  Logo,
+  FormRow,
+  Navbar,
+  SmallSidebar,
+  BigSidebar,
+  FormRowSelect,
+  Job,
+  JobsContainer,
+  SearchContainer,
+}
