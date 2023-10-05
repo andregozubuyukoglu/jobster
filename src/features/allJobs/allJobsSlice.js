51beba0eb3c7694077461d22ca41c05c00f40aa4
Andre <andregozubuyuk@gmail.com>
2023-10-05 21:48:09 +0300

    Updated files

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -60,6 +60,9 @@ const allJobsSlice = createSlice({
     clearFilters: (state) => {
       return { ...state, ...initialFiltersState }
     },
+    changePage: (state, { payload }) => {
+      state.page = payload
+    },
   },
   extraReducers: {
     [getAllJobs.pending]: (state) => {
@@ -68,6 +71,8 @@ const allJobsSlice = createSlice({
     [getAllJobs.fulfilled]: (state, { payload }) => {
       state.isLoading = false
       state.jobs = payload.jobs
+      state.numOfPages = payload.numOfPages
+      state.totalJobs = payload.totalJobs
     },
     [getAllJobs.rejected]: (state, { payload }) => {
       state.isLoading = false
@@ -88,6 +93,11 @@ const allJobsSlice = createSlice({
   },
 })
 
-export const { showLoading, hideLoading, handleChange, clearFilters } =
-  allJobsSlice.actions
+export const {
+  showLoading,
+  hideLoading,
+  handleChange,
+  clearFilters,
+  changePage,
+} = allJobsSlice.actions
 export default allJobsSlice.reducer
